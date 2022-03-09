2090ddfc20ec50b73b7b8a0b51680a325a8da1c5
Aviel Gross <avielgross@gmail.com>
2022-03-08 20:29:05 -0500

    move hud, lines, ground, to sprites

--- source/main.lua
+++ source/main.lua
@@ -8,34 +8,61 @@ import 'line'
 local gfx <const> = playdate.graphics
 
 local player = nil
+local hud = nil
+local ground = nil
+local linesSprites = nil
 
 local lines = {}
+
+local dx = 0
+local dy = 0
 
 function gameSetup()
 	local playerImg = gfx.image.new("images/player")
 	assert(playerImg)
 	
 	player = gfx.sprite.new(playerImg)
-	player:setZIndex(100)
 	player:moveTo(200,30)
 	player:setRotation(90)
 	player:add()
 	
-	gfx.sprite.setBackgroundDrawingCallback(
-		function( x, y, width, height )
-			gfx.setClipRect(x, y, width, height)
-			
-			gfx.setColor(gfx.kColorBlack)
-			gfx.setDitherPattern(0.8, gfx.image.kDitherTypeScreen)
-			gfx.fillRect(0, 60, 400, 180)
-			
-			for key, _ in pairs(lines) do
-				lines[key]:draw()
-			end
-			
-			gfx.clearClipRect()
+	ground = gfx.sprite.new()
+	ground:setZIndex(-32768)
+	ground:setSize(400, 240)
+	ground:setCenter(0,0)
+	ground:moveTo(0, 0)
+	ground:setIgnoresDrawOffset(true)
+	ground.draw = function(s, x, y, width, height)
+		gfx.setColor(gfx.kColorBlack)
+		gfx.setDitherPattern(0.8, gfx.image.kDitherTypeScreen)
+		gfx.fillRect(0, 0, width, height)
+	end
+	ground:add()
+	
+	linesSprite = gfx.sprite.new()
+	linesSprite:setSize(400,240) -- sprite per line
+	linesSprite:setCenter(0,0)
+	linesSprite:moveTo(0,0)
+	linesSprite.draw = function(s, x, y, width, height)
+		for key, _ in pairs(lines) do
+			lines[key]:draw()
 		end
-	)
+	end
+	linesSprite:add()
+	
+	hud = gfx.sprite.new()
+	hud:setSize(140, 60)
+	hud:setCenter(0, 0)
+	hud:moveTo(0, 0)
+	hud:setIgnoresDrawOffset(true)	
+	hud.draw = function(s, x, y, width, height)
+		gfx.setColor(playdate.graphics.kColorWhite)
+		gfx.fillRect(x, y, width, height)
+		gfx.drawText("dx " .. dx, 2,2)
+		gfx.drawText("dy " .. dy, 2,22)
+		gfx.drawText("lines " .. #lines, 2, 42)
+	end
+	hud:add()
 end
 
 gameSetup()
@@ -54,8 +81,6 @@ function playdate.cranked(change, acceleratedChange)
 end
 
 function playdate.update()
-	local dx = 0
-	local dy = 0
 	if moving == 1 then
 		local angle = player:getRotation()
 		dx = math.cos(math.rad(angle))
@@ -69,15 +94,18 @@ function playdate.update()
 
 		local line = Line:new(fx, fy, tx, ty)
 		table.insert(lines, line)
+		
+		hud:markDirty()
+		linesSprite:markDirty()
 	end
 	
-	gfx.sprite.update()
+	-- local _, y = player:getPosition()
+	-- if y-150 > 0 then
+	-- 	local offset = -(y-150)
+	-- 	gfx.setDrawOffset(0,offset)
+	-- end
 	
-	gfx.setColor(playdate.graphics.kColorWhite)
-	gfx.fillRect(0,0,140,60)
-	gfx.drawText("dx " .. dx, 2,2)
-	gfx.drawText("dy " .. dy, 2,22)
-	gfx.drawText("lines " .. #lines, 2, 42)
+	gfx.sprite.update()
 	
 	playdate.timer.updateTimers()
 end
