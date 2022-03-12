dc1be9dcb67ed5572e592cf6cc842d4cd61925b5
Aviel Gross <avielgross@gmail.com>
2022-03-12 18:51:34 -0500

    relative size for alert

--- source/alert.lua
+++ source/alert.lua
@@ -15,7 +15,10 @@ function Alert:init()
 	self.kAlertContinueTryAgain = 2
 	self.kAlertContinueNext = 3
 
-	self:setSize(screenW, screenH)
+	local w = 240
+	local h = 100
+	self:setSize(w, h)
+	self:moveTo(screenW/2 - w/2, screenH/2 - h/2 + 20)
 	self:setCenter(0,0)
 	self:add()
 end
@@ -24,17 +27,8 @@ function Alert:set(message, continue, callback)
 	self:markDirty()
 end
 
-function Alert:draw(_,_,_,_)	
-	if self.alertMessage then
-		local sw = screenW -- screen width
-		local sh = screenH -- screen height
-	
-		local w = 240
-		local h = 100
-		
-		local x = sw/2 - w/2
-		local y = sh/2 - h/2 + 20
-		
+function Alert:draw(x,y,w,h)	
+	if self.alertMessage then		
 		local r = 8 -- corner radius
 		gfx.setColor(gfx.kColorWhite)
 		gfx.fillRoundRect(x,y,w,h,r)
