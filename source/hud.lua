18be386aee9969ca7ce9dbf3859e05c0d42998d1
Aviel Gross <avielgross@gmail.com>
2022-03-22 19:41:37 -0400

    score and distance in hud (debug mode off)

--- source/hud.lua
+++ source/hud.lua
@@ -7,7 +7,7 @@ function Hud:init()
 	Hud.super.init(self)
 	
 	self:setZIndex(999)
-	self:setSize(400, 14)
+	self:setSize(400, 16)
 	self:setCenter(0, 0)
 	self:moveTo(0, 0)
 	self:setIgnoresDrawOffset(true)	
@@ -22,18 +22,51 @@ function Hud:draw(x, y, width, height)
 	gfx.setColor(gfx.kColorBlack)
 	gfx.fillRect(x, y, width, height)
 	playdate.graphics.setImageDrawMode(playdate.graphics.kDrawModeFillWhite)
-	if self.scorpionLine then
-		gfx.drawText("Lines " .. self.numLines .. " | " .. self.numLines - self.scorpionLine, 0, 0)
+	
+	local dy = 2 -- draw Y
+	if self.debugMode then
+		if self.scorpionLine then
+			gfx.drawText("Lines " .. self.numLines .. " | " .. self.numLines - self.scorpionLine, 0, dy)
+		else
+			gfx.drawText("Lines " .. self.numLines, 0, dy)
+		end
+		gfx.drawText("Player " .. self.playerX .. ", " .. self.playerY, 80, dy)
+		gfx.drawText("dx " .. self.dx, 260,dy)
+		gfx.drawText("dy " .. self.dy, 330,dy)
 	else
-		gfx.drawText("Lines " .. self.numLines, 0, 0)
+		local adjustedY = self.playerY - 60
+		local digging = adjustedY > 0
+		if digging then
+			local cm = adjustedY / 100 -- each pixel is a millimeter...
+			gfx.drawText("Depth: " .. math.floor(cm) .. "cm", 2, dy)
+		else
+			gfx.drawText("Start digging!", 2, dy)
+		end
+		
+		if self.scorpionLine then
+			local kLinesWhenScorpionHitPlayer = 15
+			local distance = self.numLines - self.scorpionLine - kLinesWhenScorpionHitPlayer
+			distance = math.min(60, distance)
+			local careful = distance < 16
+			local text = ""
+			for i = 1, distance do
+				if i % 3 == 0 then
+					local tick = "I"
+					if careful then tick = "!  " end
+					text = text .. "*" .. tick .. "* "
+				end
+			end
+			if careful and distance > 0 then
+				gfx.drawText("*Careful*", 232, dy)
+			end	
+			gfx.drawText(text, 280, dy)
+		end
 	end
-	gfx.drawText("Player " .. self.playerX .. ", " .. self.playerY, 80, 0)
-	gfx.drawText("dx " .. self.dx, 260,0)
-	gfx.drawText("dy " .. self.dy, 330,0)
 	gfx.clearClipRect()
 end
 
 function Hud:reset()
+	self.debugMode = false
 	self.dx = 0
 	self.dy = 0
 	self.playerY = 0
