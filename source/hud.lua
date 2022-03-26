b07a89fdbf32c2d6ffc00b7b7ad157d4a1ec34c7
Aviel Gross <avielgross@gmail.com>
2022-03-26 10:06:11 -0400

    refactor Hud

--- source/hud.lua
+++ source/hud.lua
@@ -48,59 +48,71 @@ function Hud:shakeBellyText()
 	t = shakeTimer
 end
 
+local dy = 2 -- draw Y
+
+function Hud:drawDebugMode()
+	if self.scorpionLine then
+		gfx.drawText("Lines " .. self.numLines .. " | " .. self.numLines - self.scorpionLine, 0, dy)
+	else
+		gfx.drawText("Lines " .. self.numLines, 0, dy)
+	end
+	gfx.drawText("Player " .. self.playerX .. ", " .. self.playerY, 80, dy)
+	gfx.drawText("dx " .. self.dx, 260,dy)
+	gfx.drawText("dy " .. self.dy, 330,dy)
+end
+
+function Hud:drawDepthAndFood()
+	local adjustedY = self.playerY - 60
+	local digging = adjustedY > 0
+	if digging then
+		local cm = adjustedY / 100 -- each pixel is a millimeter...
+		gfx.drawText("Depth: *" .. math.floor(cm) .. " cm*", 2, dy)
+		
+		local bt = self:bellyText()
+		local spaces = ""
+		for i = 1, offsetBellyText do
+			if i % 3 == 0 then
+				local tick = " "
+				spaces = spaces .. tick
+			end
+		end
+		gfx.drawText("Food: *" .. self.numFoods .. " noms*", 80, dy)
+		gfx.drawText(spaces .. bt, 180, dy)
+	else
+		gfx.drawText("Start digging!", 2, dy)
+	end
+end
+
+function Hud:drawScorpionDistance()
+	local distance = self.numLines - self.scorpionLine - kLinesWhenScorpionHitPlayer
+	distance = math.min(kAboveGroundSpace, distance)
+	local careful = distance < 16
+	local text = ""
+	for i = 1, distance do
+		if i % 3 == 0 then
+			local tick = "I"
+			if careful then tick = "!  " end
+			text = text .. "*" .. tick .. "* "
+		end
+	end
+	if careful and distance > 0 then
+		gfx.drawText("*Careful*", 232, dy)
+	end	
+	gfx.drawText(text, 280, dy)
+end
+
 function Hud:draw(x, y, width, height)
 	gfx.setClipRect(x, y, width, height)
 	gfx.setColor(gfx.kColorBlack)
 	gfx.fillRect(x, y, width, height)
 	playdate.graphics.setImageDrawMode(playdate.graphics.kDrawModeFillWhite)
 	
-	local dy = 2 -- draw Y
 	if self.debugMode then
-		if self.scorpionLine then
-			gfx.drawText("Lines " .. self.numLines .. " | " .. self.numLines - self.scorpionLine, 0, dy)
-		else
-			gfx.drawText("Lines " .. self.numLines, 0, dy)
-		end
-		gfx.drawText("Player " .. self.playerX .. ", " .. self.playerY, 80, dy)
-		gfx.drawText("dx " .. self.dx, 260,dy)
-		gfx.drawText("dy " .. self.dy, 330,dy)
+		self:drawDebugMode()
 	else
-		local adjustedY = self.playerY - 60
-		local digging = adjustedY > 0
-		if digging then
-			local cm = adjustedY / 100 -- each pixel is a millimeter...
-			gfx.drawText("Depth: *" .. math.floor(cm) .. " cm*", 2, dy)
-			
-			local bt = self:bellyText()
-			local spaces = ""
-			for i = 1, offsetBellyText do
-				if i % 3 == 0 then
-					local tick = " "
-					spaces = spaces .. tick
-				end
-			end
-			gfx.drawText("Food: *" .. self.numFoods .. " noms*", 80, dy)
-			gfx.drawText(spaces .. bt, 180, dy)
-		else
-			gfx.drawText("Start digging!", 2, dy)
-		end
-		
+		self:drawDepthAndFood()
 		if self.scorpionLine then
-			local distance = self.numLines - self.scorpionLine - kLinesWhenScorpionHitPlayer
-			distance = math.min(kAboveGroundSpace, distance)
-			local careful = distance < 16
-			local text = ""
-			for i = 1, distance do
-				if i % 3 == 0 then
-					local tick = "I"
-					if careful then tick = "!  " end
-					text = text .. "*" .. tick .. "* "
-				end
-			end
-			if careful and distance > 0 then
-				gfx.drawText("*Careful*", 232, dy)
-			end	
-			gfx.drawText(text, 280, dy)
+			self:drawScorpionDistance()
 		end
 	end
 	gfx.clearClipRect()
